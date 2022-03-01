b737df39a9f94a10412a8ee1c1f9893902434067
ChrisCho <chrischo2012@gmail.com>
2022-03-01 16:50:46 +0900

    front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import "./App.css";
+import Home from './pages';
+import Signin from './pages/signin'
 
-class App extends Component {
-  render() {
+function App() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/signin" element={<Signin/>} />
+        </Routes>
+      </Router>
     );
   }
-}
+
 
 export default App;
